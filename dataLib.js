702d9a28797267bc417b3c77f5a1b306b3ef02cb
Jogel <git@jogel.com>
2019-03-03 08:03:20 +0100

    add webpack and react

--- dataLib.js
+++ dataLib.js
@@ -1,2 +1,2 @@
 // const inventory_sets = require('csv-loader!../data/inventory_sets.csv');
-const inventory_sets = require('../data/inventory_sets.csv');
+// const inventory_sets = require('../data/inventory_sets.csv');
